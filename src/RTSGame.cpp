5d3697f3597c423d3afa6b659e5d94a88487592d
costin88boss <costin211.boss@gmail.com>
2025-01-22 23:36:17 +0200

    clang format spacing

--- src/RTSGame.cpp
+++ src/RTSGame.cpp
@@ -1,15 +1,16 @@
 ﻿#include <SFML/Graphics.hpp>
 
 int main() {
-  auto window = sf::RenderWindow(sf::VideoMode({720u, 480u}), "Hello, SFML!");
+    auto window = sf::RenderWindow(sf::VideoMode({720u, 480u}), "Hello, SFML!");
 
-  while (window.isOpen()) {
-    while (const std::optional event = window.pollEvent()) {
-      if (!event->is<sf::Event::Closed>()) break;
-      window.close();
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (!event->is<sf::Event::Closed>())
+                break;
+            window.close();
+        }
+
+        window.clear();
+        window.display();
     }
-
-    window.clear();
-    window.display();
-  }
 }
